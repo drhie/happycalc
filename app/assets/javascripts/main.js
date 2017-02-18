237943c6f5513ff5f78f66bf222c742c9bce8894
drhie <rhie.david@gmail.com>
2017-02-18 17:24:46 -0500

    Start ajax call code

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,12 +6,14 @@
 // })
 
 $(document).ready( function() {
-  $('.area-example').hover(function() {
-    $(this).css('border', '1px solid blue');
-    $(this).css('font-weight', '900');
+  var name;
+  var importance;
+  var satisfaction;
+
+  $('.area-example, #add').hover(function() {
+    $(this).css('border', '1px solid yellow').css('font-weight', '900');
   }, function() {
-    $(this).css('border', '1px solid gray');
-    $(this).css('font-weight', '500');
+    $(this).css('border', '1px solid gray').css('font-weight', '500');
   });
 
   $('.area-example').on('click', function() {
@@ -20,7 +22,7 @@ $(document).ready( function() {
   })
 
   $('.num-button, .sub-button, .clear-button').hover(function() {
-    $(this).css('border', '1px solid blue');
+    $(this).css('border', '1px solid yellow');
     $(this).css('font-weight', '900');
   }, function() {
     $(this).css('border', '1px solid gray');
@@ -42,4 +44,39 @@ $(document).ready( function() {
   $('.clear-button').on('click', function() {
     $('.number-field').html("");
   });
+
+  var slider = $('input#satisfaction');
+  slider.on("input", function() {
+    $('.output').html("I'm " + Math.floor((this.value/20)*100) + '% happy about it.');
+  })
+
+  $('#here').on('click', function() {
+    if ($('input#area').val() !== "") {
+      if($('.screen').html() === "...") {
+        // $.ajax({
+        //   url: 'http://localhost:3000/areas',
+        //   method: 'POST',
+        //   data: {
+        //     name: $('input#area').val(),
+        //     importance: parseInt($('.number-field').html()),
+        //     satisfaction: parseInt($('input#satisfaction').val()),
+        //   }
+        // }).done(function(data) {
+        //   console.log(data)
+        // });
+        $('.screen').html("(" + $('input#area').val().toLowerCase() + ")");
+        reset();
+      } else {
+        $('.screen').append('<br>');
+        $('.screen').append("+ (" + $('input#area').val().toLowerCase() + ")");
+        reset();
+      }
+    }
+  });
+
+  var reset = function() {
+    $('input#area').val("");
+    $('.number-field').html("From 1 to 10").css('color', 'darkgray');
+    $('.output').html("Move the slider!");
+  }
 });
